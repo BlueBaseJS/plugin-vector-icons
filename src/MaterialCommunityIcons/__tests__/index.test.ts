769e0924e754f2816f1d21f12fc4bd7dc65d3e19
adnan1naeem <adnan1naeem@gmail.com>
2019-04-20 15:18:11 +0500

    test(Material-Community Icons): fixed testcases of Material-Community Icons

--- src/MaterialCommunityIcons/__tests__/index.test.ts
+++ src/MaterialCommunityIcons/__tests__/index.test.ts
@@ -7,6 +7,6 @@ test('Plugin should be correctly registered', async () => {
 	await BB.boot();
 
 	expect(BB.Plugins.has('vector-icons-material-community-icons')).toBe(true);
-	expect(BB.Fonts.has('Material Icons')).toBe(true);
+	expect(BB.Fonts.has('MaterialCommunity Icons')).toBe(true);
 	expect(BB.Components.has('Icon')).toBe(true);
 });
