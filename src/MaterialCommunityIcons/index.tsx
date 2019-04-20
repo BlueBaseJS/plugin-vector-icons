35704d7d8aed468f4458c0d184e9183db7f5891c
adnan1naeem <adnan1naeem@gmail.com>
2019-04-20 21:17:25 +0500

    feat(CommunityIcons): community Icons   working fine

--- src/MaterialCommunityIcons/index.tsx
+++ src/MaterialCommunityIcons/index.tsx
@@ -1,13 +1,16 @@
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
 import { createPlugin } from '@bluebase/core';
-
 export default createPlugin({
 	description: 'A BlueBase port of the React Native Vector Icons project',
 	key: 'vector-icons-material-community-icons',
 	name: 'Vector Icons - Material Community Icons',
 	version: '1.0.0',
 
-	components: { Icon },
+	components: {
+		// 'HomeScreen': () => <Icon name="delete" size={100} color="green" />,
+		Icon,
+	},
 
 	fonts: {
 		'MaterialCommunity Icons': require('react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf'),
