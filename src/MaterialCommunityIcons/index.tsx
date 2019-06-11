041f909011381cd274b15b5f299a52f2cdcb2b84
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-06-11 16:12:39 +0500

    feat: Compatible with React Native 0.59.8

--- src/MaterialCommunityIcons/index.tsx
+++ src/MaterialCommunityIcons/index.tsx
@@ -1,13 +1,11 @@
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf';
 import { createPlugin } from '@bluebase/core';
 
-// // TODO: This is a temp fix for:
-// // https://github.com/react-native-community/cli/issues/228
-// // tslint:disable-next-line: no-var-requires
-// const Font = require('../../MaterialCommunityIcons.ttf');
-
-// // const Font = require('react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf');
+// TODO: This is a temp fix for:
+// https://github.com/react-native-community/cli/issues/228
+// import MaterialCommunityIcons from 'react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf';
+// tslint:disable-next-line: no-var-requires
+const MaterialCommunityIcons = require('../../Fonts/MaterialCommunityIcons.ttf');
 
 export default createPlugin({
 	description: 'A BlueBase port of the React Native Vector Icons project',
