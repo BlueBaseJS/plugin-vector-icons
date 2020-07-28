94cdc693516905553aeb1da4295c6f71595eca20
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-07-28 19:45:59 +0500

    feat(package): Updated lib to V7

--- src/MaterialIcons/index.ts
+++ src/MaterialIcons/index.ts
@@ -1,11 +1,6 @@
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialIcons from 'react-native-vector-icons/Fonts/MaterialIcons.ttf';
 import { createPlugin } from '@bluebase/core';
-
-// TODO: This is a temp fix for:
-// https://github.com/react-native-community/cli/issues/228
-// import MaterialIcons from 'react-native-vector-icons/Fonts/MaterialIcons.ttf';
-// tslint:disable-next-line: no-var-requires
-const MaterialIcons = require('../../Fonts/MaterialIcons.ttf');
 
 export default createPlugin({
 	description: 'A BlueBase port of the React Native Vector Icons project',
