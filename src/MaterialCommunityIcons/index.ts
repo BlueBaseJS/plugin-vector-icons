04ca0407e14b14c5b82167dfce9a7f26edb48ab0
Abdul Rehman Talat <rehman.talat@gmail.com>
2024-04-04 21:43:52 +0500

    fix: Fixed icon issue on ios

--- src/MaterialCommunityIcons/index.ts
+++ src/MaterialCommunityIcons/index.ts
@@ -1,20 +1,21 @@
 import { createPlugin } from '@bluebase/core';
 
 import MaterialCommunityIcons from '../../Fonts/MaterialCommunityIcons.ttf';
+import { VERSION } from '../version';
 import { Icon } from './BlueBaseIcon';
 
 export default createPlugin({
 	description: 'A BlueBase port of the React Native Vector Icons project',
 	key: 'vector-icons-material-community-icons',
 	name: 'Vector Icons - Material Community Icons',
-	version: '1.0.0',
+	version: VERSION,
 
 	components: { Icon: Icon as any },
 
 	fonts: {
 		// react native
 		'Material Design Icons': MaterialCommunityIcons,
-		'material-community': MaterialCommunityIcons,
+		// 'material-community': MaterialCommunityIcons,
 
 		// web
 		MaterialCommunityIcons,
--- src/MaterialIcons/index.ts
+++ src/MaterialIcons/index.ts
@@ -1,13 +1,14 @@
 import { createPlugin } from '@bluebase/core';
 
 import MaterialIcons from '../../Fonts/MaterialIcons.ttf';
+import { VERSION } from '../version';
 import { Icon } from './BlueBaseIcon';
 
 export default createPlugin({
 	description: 'A BlueBase port of the React Native Vector Icons project',
 	key: 'vector-icons-material-icons',
 	name: 'Vector Icons - Material Icons',
-	version: '1.0.0',
+	version: VERSION,
 
 	components: { Icon: Icon as any },
 
